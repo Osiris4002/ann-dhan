e47bf5e77fd69e6a3bb0b155c06a86343e909a32
Shivam Kumar <shivam0204official@gmail.com>
2025-09-23 08:26:16 +0530

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import ChatbotScreen from "../src/screens/chatbot";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome</Text>
+     <View style={styles.container}>
+      <Text style={styles.header}>Ann Dhan Chatbot</Text>
+      <ChatbotScreen /> {/* 👈 Render the chatbot component here */}
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
